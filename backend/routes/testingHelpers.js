57c284f5e6c75a12741418ae0105bdb93f4b6a64
Jetusan <jyttivann70@gmail.com>
2025-10-24 01:51:39 +0800

    change 59a

--- backend/routes/testingHelpers.js
+++ backend/routes/testingHelpers.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../config/database');
+const pool = require('../config/db');
 const { authenticateJWT } = require('../middleware/auth');
 
 // Clear duplicate image records for testing
